657370561df0aa8863dfe1f3a7d00973c58345d5
Rogier Lommers <rogier@lommers.org>
2017-01-02 21:37:24 +0100

    Added test

--- logrus_redis_test.go
+++ logrus_redis_test.go
@@ -1,1 +1,21 @@
 package logredis
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewHookFunc(t *testing.T) {
+	hook, err := NewHook("redishost", "key", "format", "appname", "hostname", "password", 1)
+	if hook != nil {
+		t.Fatalf("TestNewHookFunc, expected no hook, got hook: %s", hook)
+	}
+
+	if err == nil {
+		t.Fatalf("TestNewHookFunc, expected %q, got %s.", "unknown message format", err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
