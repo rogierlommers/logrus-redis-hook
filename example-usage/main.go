fba330af36b318e0c457e5a950692df60872d5a9
Jinhyung Kim <jinhyung@whimr.net>
2017-08-07 09:57:13 +0900

    Update logrus import path

--- example-usage/main.go
+++ example-usage/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"io/ioutil"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/rogierlommers/logrus-redis-hook"
 )
 
--- logrus_redis.go
+++ logrus_redis.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/garyburd/redigo/redis"
 )
 
