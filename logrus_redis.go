1af1b10d4ca11c572c31158284168d56e727d6b9
ycs <383451140@qq.com>
2018-02-01 10:39:58 +0800

    FIX:dial error if need authentication and option DialDatabase not 0

--- logrus_redis.go
+++ logrus_redis.go
@@ -156,19 +156,10 @@ func newRedisConnectionPool(server, password string, port int, db int) *redis.Po
 		MaxIdle:     3,
 		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", hostPort, redis.DialDatabase(db))
+			c, err := redis.Dial("tcp", hostPort, redis.DialDatabase(db),redis.DialPassword(password))
 			if err != nil {
 				return nil, err
 			}
-
-			// In case redis needs authentication
-			if password != "" {
-				if _, err := c.Do("AUTH", password); err != nil {
-					c.Close()
-					return nil, err
-				}
-			}
-
 			return c, err
 		},
 		TestOnBorrow: func(c redis.Conn, t time.Time) error {
